a8ed6fcdbf80e936d160ea2b0ce03f5cc35658a1
Dmytro Los <chicanodima@gmail.com>
2022-03-22 16:20:23 +0200

    step 4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,18 @@
-import { test, expect } from "@jest/globals";
-import { fileURLToPath } from "url";
-import path, { dirname } from "path";
-import { readFileSync } from "fs";
-import gendiff from "../src/gendiff.js";
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+import gendiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, "..", "__fixtures__", filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test("gendiff Test", () => {
-  const filePath1 = getFixturePath("file1.json");
-  const filePath2 = getFixturePath("file2.json");
-  const expected = readFileSync(getFixturePath("expected.txt"), "utf-8");
+test('gendiff Test', () => {
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file2.json');
+  const expected = readFileSync(getFixturePath('expected.txt'), 'utf-8');
 
   expect(gendiff(filePath1, filePath2)).toBe(expected);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,12 +1,12 @@
-import { readFileSync } from "fs";
-import pkg from "lodash";
+import { readFileSync } from 'fs';
+import pkg from 'lodash';
 
 function objToString(obj) {
-  let str = "";
+  let str = '';
   const keys = Object.keys(obj);
   const values = Object.values(obj);
   for (let i = 0; i < keys.length; i += 1) {
-    str += `${keys[i]} :  ${values[i]}\n`;
+    str += `${keys[i]}: ${values[i]}\n`;
   }
 
   return `{\n${str}}`;
@@ -21,24 +21,24 @@ function getObjcetsCompared(object1, object2) {
   const result = {};
   unitedKeys.forEach((key) => {
     if (has(object1, key) && !has(object2, key)) {
-      result[` - ${key}`] = object1[key];
+      result[`  - ${key}`] = object1[key];
     } else if (has(object2, key) && !has(object1, key)) {
-      result[` + ${key}`] = object2[key];
+      result[`  + ${key}`] = object2[key];
     } else if (object1[key] !== object2[key]) {
-      result[` - ${key}`] = object1[key];
-      result[` + ${key}`] = object2[key];
+      result[`  - ${key}`] = object1[key];
+      result[`  + ${key}`] = object2[key];
     } else {
-      result[`   ${key}`] = object1[key];
+      result[`    ${key}`] = object1[key];
     }
   });
-  console.log(objToString(result));
+  // console.log(objToString(result));
   return objToString(result);
 }
 
 export default function gendiff(filepath1, filepath2) {
-  const dataFile1 = readFileSync(filepath1, "utf8");
-  const dataFile2 = readFileSync(filepath2, "utf8");
+  const dataFile1 = readFileSync(filepath1, 'utf8');
+  const dataFile2 = readFileSync(filepath2, 'utf8');
   const parsedData1 = JSON.parse(dataFile1);
   const parsedData2 = JSON.parse(dataFile2);
-  getObjcetsCompared(parsedData1, parsedData2);
+  return getObjcetsCompared(parsedData1, parsedData2);
 }

332fea8df57db8dd524d8a2d1a612287dc9cd239
Dmytro Los <chicanodima@gmail.com>
2022-07-13 14:50:08 +0300

    fix parses

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import gendiff from "../src/index.js";
+
+const program = new Command();
+
+program
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .version("0.8.0")
+  .option("-f, --format <type>", "output format", "stylish")
+  .arguments("<filepath1> <filepath2>")
+  .action((file1, file2) => {
+    const { format } = program.opts();
+    console.log(gendiff(file1, file2, format));
+  });
+
+program.parse();
---
+++ src/formatters/formatter.js
@@ -0,0 +1,20 @@
+import stylish from "./stylish.js";
+import plain from "./plain.js";
+
+const formatter = (tree, format) => {
+  switch (format) {
+    case "stylish":
+      return stylish(tree);
+
+    case "plain":
+      return plain(tree);
+
+    case "json":
+      return JSON.stringify(tree, null, " ");
+
+    default:
+      throw new Error(`Invalid this exist ${format}`);
+  }
+};
+
+export default formatter;

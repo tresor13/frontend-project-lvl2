b1eb7a4035b19314cf24e8eb72dc9332b46d3c77
Dmytro Los <chicanodima@gmail.com>
2022-07-28 15:08:39 +0300

    corrections

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -1,46 +1,46 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-const diff = (file1, file2) => {
+const compareFiles = (file1, file2) => {
   const iter = (node1, node2) => {
     const keys = _.sortBy(_.union(_.keys(node1), _.keys(node2)));
     const result = keys.map((key) => {
       if (!_.has(node1, key)) {
         return {
           name: key,
-          action: 'added',
+          action: "added",
           value: node2[key],
         };
       }
       if (!_.has(node2, key)) {
         return {
           name: key,
-          action: 'removed',
+          action: "removed",
           value: node1[key],
         };
       }
       if (_.isObject(node1[key]) && _.isObject(node2[key])) {
         return {
           name: key,
-          action: 'nested',
+          action: "nested",
           children: iter(node1[key], node2[key]),
         };
       }
       if (!_.isEqual(node1[key], node2[key])) {
         return {
           name: key,
-          action: 'updated',
+          action: "updated",
           value1: node1[key],
           value2: node2[key],
         };
       }
       return {
         name: key,
-        action: 'save',
+        action: "save",
         value: node1[key],
       };
     });
     return result;
   };
-  return { name: 'tree', action: 'root', children: iter(file1, file2) };
+  return { name: "tree", action: "root", children: iter(file1, file2) };
 };
-export default diff;
+export default compareFiles;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import path from 'path';
-import * as fs from 'fs';
-import diff from './diff.js';
-import formatter from './formatters/formatter.js';
-import parser from './funcParse.js';
+import path from "path";
+import * as fs from "fs";
+import compareFiles from "./compareFiles.js";
+import formatter from "./formatters/formatter.js";
+import parser from "./parser.js";
 
-const extractData = (way) => {
-  const format = path.extname(way);
-  const obj = fs.readFileSync(way, 'utf-8');
+const extractData = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const format = path.extname(filepath).slice(1);
+  const obj = fs.readFileSync(absolutePath, "utf-8");
   return parser(obj, format);
 };
 
-const gendiff = (file1, file2, format = 'stylish') => {
+const gendiff = (file1, file2, format = "stylish") => {
   const data1 = extractData(file1);
   const data2 = extractData(file2);
-  const callDiff = diff(data1, data2);
+  const callDiff = compareFiles(data1, data2);
   return formatter(callDiff, format);
 };
+
 export default gendiff;

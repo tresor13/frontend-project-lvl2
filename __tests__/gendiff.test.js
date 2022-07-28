a0867a47f0d805dba7a3d6ff3a59dc01bbfe38fd
Dmytro Los <chicanodima@gmail.com>
2022-07-28 16:39:57 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,31 +1,36 @@
-import { expect, test } from '@jest/globals';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import * as fs from 'fs';
-import gendiff from '../src/index.js';
+import { expect, test } from "@jest/globals";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import * as fs from "fs";
+import gendiff from "../src/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(filename, 'utf-8');
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
+const readFile = (filename) => fs.readFileSync(filename, "utf-8");
 
-const expectStylish = readFile(getFixturePath('stylish.expect.txt'));
-const expectPlain = readFile(getFixturePath('plain.expect.txt'));
-const yamlFile1 = getFixturePath('file1.yaml');
-const yamlFile2 = getFixturePath('file2.yaml');
-const jsonFile1 = getFixturePath('file1.json');
-const jsonFile2 = getFixturePath('file2.json');
+const expectStylish = readFile(getFixturePath("stylish.expect.txt"));
+const expectPlain = readFile(getFixturePath("plain.expect.txt"));
 
-test.each([
-  [yamlFile1, yamlFile2, expectStylish],
-  [jsonFile1, jsonFile2, expectStylish],
-])('Test format stylish (%s, %s)', (file1, file2, expected) => {
-  expect(gendiff(file1, file2, 'stylish')).toBe(expected);
-});
+test.each(["yaml", "json"])("Test format stylish (%s, %s)", (ext) => {
+  expect(
+    gendiff(
+      getFixturePath(`file1.${ext}`),
+      getFixturePath(`file2.${ext}`),
+      "stylish"
+    )
+  ).toBe(expectStylish);
 
-test.each([
-  [yamlFile1, yamlFile2, expectPlain],
-  [jsonFile1, jsonFile2, expectPlain],
-])('Test format plain (%s, %s)', (file1, file2, expected) => {
-  expect(gendiff(file1, file2, 'plain')).toBe(expected);
+  expect(
+    gendiff(
+      getFixturePath(`file1.${ext}`),
+      getFixturePath(`file2.${ext}`),
+      "plain"
+    )
+  ).toBe(expectPlain);
+
+  expect(
+    gendiff(getFixturePath(`file1.${ext}`), getFixturePath(`file2.${ext}`))
+  ).toBe(expectStylish);
 });
